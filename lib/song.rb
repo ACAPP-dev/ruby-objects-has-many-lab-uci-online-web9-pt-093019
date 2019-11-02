f2a93adc3c58a1bd304f250b8b5c34f0dbf341ce
Andrew Capp <acapp909@gmail.com>
2019-11-02 06:35:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,20 @@ require 'pry'
 
 class Song
 
+  @@all = []
 
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @all
+  end
 
 
 
 
 end
-
-
-  
