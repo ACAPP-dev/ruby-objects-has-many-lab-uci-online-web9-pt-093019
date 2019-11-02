04e2b324abc3c60e85f7aee6688f50fcebc1670a
Andrew Capp <acapp909@gmail.com>
2019-11-02 07:07:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,13 +4,17 @@ class Song
 
   @@all = []
 
-  attr_accessor :name, :artist, :artist_name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
     @@all << self
   end
 
+  def artist_name
+    self.artist.name
+  end
+  
   def self.all
     @all
   end
